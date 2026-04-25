8e97b5db523aa6eaefa989567724e6d1b590f97e
Tanaka Musungare Wonder <tanakam@asagroup.co.zw>
2026-04-25 09:15:51 +0200

    adding a ticket lookup functionality

--- src/app/apiBaseUrl.ts
+++ src/app/apiBaseUrl.ts
@@ -1,5 +1,3 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL ;
-
-console.log("API Base URL:", BASE_URL);
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8082";
 
 export default BASE_URL;
